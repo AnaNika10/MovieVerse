6727fafa95e8468820b656280580c33e92f789ab
marijal74 <lakic.marija98@gmail.com>
2022-04-07 12:38:56 +0200

    [file/feature] Remove execute permission - Linux

--- server/MovieVerseServer/Services/File/Controllers/FileController.cs
+++ server/MovieVerseServer/Services/File/Controllers/FileController.cs
@@ -10,7 +10,8 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Hosting;
 using System.Security.Principal;
 using System.Security.AccessControl;
-
+using System.Runtime.InteropServices;
+using Mono.Unix;
 namespace File.Controllers
 {
     [ApiController]
@@ -31,15 +32,38 @@ namespace File.Controllers
         }
 
 
-        private void removeExecutePermissions(string dirPath)
+        private void removeExecutePermissionsWindows(string dirPath)
         {
             DirectoryInfo dInfo = new DirectoryInfo(dirPath);
-            DirectorySecurity dSecurity = dInfo.GetAccessControl();
+                DirectorySecurity dSecurity = dInfo.GetAccessControl();
 
-            dSecurity.RemoveAccessRule(new FileSystemAccessRule("Everyone", 
-                                                                FileSystemRights.ExecuteFile, 
-                                                                AccessControlType.Allow));
-            dInfo.SetAccessControl(dSecurity);
+                dSecurity.RemoveAccessRule(new FileSystemAccessRule("Everyone", 
+                                                                    FileSystemRights.ExecuteFile, 
+                                                                    AccessControlType.Allow));
+                dInfo.SetAccessControl(dSecurity);   
+        }
+        private void removeExecutePermissionsUnix(string dirPath)
+        {
+        
+            var unixFileInfo = new Mono.Unix.UnixFileInfo(dirPath);
+            unixFileInfo.FileAccessPermissions =
+                FileAccessPermissions.UserRead | FileAccessPermissions.UserWrite | FileAccessPermissions.UserExecute
+                | FileAccessPermissions.GroupRead | FileAccessPermissions.GroupWrite
+                | FileAccessPermissions.OtherRead | FileAccessPermissions.OtherWrite;
+        }
+        private void removeExecutePermissions(string dirPath)
+        {
+            
+            if(System.Runtime.InteropServices.RuntimeInformation
+                                               .IsOSPlatform(OSPlatform.Windows))
+            {
+                 removeExecutePermissionsWindows(dirPath);
+            }
+            else
+            {
+                removeExecutePermissionsUnix(dirPath);
+            }        
+            
         }
 
         [HttpPost]
@@ -55,8 +79,7 @@ namespace File.Controllers
                 {
                     Directory.CreateDirectory(path);
                     // TODO: test on Windows: ls -l and see if x permissions are missing for every user
-                    // TODO: implement for linux
-                    // removeExecutePermissions(path);
+                    removeExecutePermissions(path);
                 }
                 // TODO: construct uniqueFileName
                 // TODO: HTML encode originalFileName
