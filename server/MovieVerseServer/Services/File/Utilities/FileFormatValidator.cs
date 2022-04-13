16532e30380e82cae4def65a7702d9b1ab8b4473
marijal74 <lakic.marija98@gmail.com>
2022-04-14 00:18:03 +0200

    [file/feat] Support .mp4 signature check

--- server/MovieVerseServer/Services/File/Utilities/FileFormatValidator.cs
+++ server/MovieVerseServer/Services/File/Utilities/FileFormatValidator.cs
@@ -12,44 +12,56 @@ namespace File.Utilities{
     public static class FileFormatValidator{
 
         private static string[] permittedFileExtensions = {".jpg", ".jpeg", ".gif", ".png", ".mkv", ".mp4"};
-        private static readonly Dictionary<string, List<byte[]>> _fileSignature = 
-                                            new Dictionary<string, List<byte[]>>
+        private static readonly Dictionary<string, Tuple<List<byte[]>, int>> _fileSignature = 
+                                            new Dictionary<string, Tuple<List<byte[]>, int>>
                                             {
-                                                { ".jpeg", new List<byte[]>
+                                                { ".jpeg", new Tuple<List<byte[]>, int>(
+                                                    new List<byte[]>
                                                     {
                                                         new byte[] { 0xFF, 0xD8, 0xFF, 0xE0 },
                                                         new byte[] { 0xFF, 0xD8, 0xFF, 0xE2 },
                                                         new byte[] { 0xFF, 0xD8, 0xFF, 0xE3 },
-                                                    }
+                                                    },
+                                                    0)
                                                 },
-                                                { ".jpg", new List<byte[]>
+                                                { ".jpg", new Tuple<List<byte[]>, int>(
+                                                    new List<byte[]>
                                                     {
                                                         new byte[] { 0xFF, 0xD8, 0xFF, 0xE0 },  	 
                                                         new byte[] { 0xFF, 0xD8, 0xFF, 0xE1 },
                                                         new byte[] { 0xFF, 0xD8, 0xFF, 0xE8 },
-                                                    }
+                                                    },
+                                                    0)
                                                 },
-                                                { ".gif", new List<byte[]>
+                                                { ".gif", new Tuple<List<byte[]>, int>(
+                                                    new List<byte[]>
                                                     {
                                                         new byte[] { 0x47, 0x49, 0x46, 0x38 }
-                                                    }
+                                                    },
+                                                    0)
                                                 },
-                                                { ".png", new List<byte[]>
+                                                { ".png", new Tuple<List<byte[]>, int>(
+                                                    new List<byte[]>
                                                     {
                                                         new byte[] { 0x89, 0x50, 0x4E, 0x47, 
                                                                      0x0D, 0x0A, 0x1A, 0x0A }
-                                                    }
+                                                    },
+                                                    0)
                                                 },
-                                                { ".mkv", new List<byte[]>
+                                                { ".mkv", new Tuple<List<byte[]>, int>(
+                                                    new List<byte[]>
                                                     {
                                                         new byte[] { 0x1A, 0x45, 0xDF, 0xA3},
-                                                    }
+                                                    },
+                                                    0)
                                                 },
-                                                { ".mp4", new List<byte[]>
+                                                { ".mp4", new Tuple<List<byte[]>, int>(
+                                                    new List<byte[]>
                                                     {
                                                         new byte[] { 0x66, 0x74, 0x79, 0x70, 
                                                                      0x69, 0x73, 0x6F, 0x6D}
-                                                    }
+                                                    },
+                                                    4)
                                                 },
                                                 
                                                 
@@ -61,7 +73,8 @@ namespace File.Utilities{
             
             using (var reader = new BinaryReader(file))
             {
-                var signatures = _fileSignature[ext];
+                var (signatures, offset) = _fileSignature[ext];
+                reader.ReadBytes(offset);
                 var headerBytes = reader.ReadBytes(signatures.Max(m => m.Length));
                 var temp = Convert.ToHexString(headerBytes.Take(4).ToArray<byte>());
                 return signatures.Any(signature => 
