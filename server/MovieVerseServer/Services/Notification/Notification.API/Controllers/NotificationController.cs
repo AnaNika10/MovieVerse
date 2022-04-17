449f3ce118d2a144119b137a010afb4e57506805
Nikola Stanojević <stanojevic.business@gmail.com>
2022-04-17 15:38:27 +0200

    Create NotificationController.cs

---
+++ server/MovieVerseServer/Services/Notification/Notification.API/Controllers/NotificationController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Notification.API.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Notification.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotificationController : ControllerBase
+    {
+        private readonly INotificationRepository _repository;
+
+        public NotificationController(INotificationRepository repository)
+        {
+            _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<Entities.Notification>), StatusCodes.Status200OK)]
+        public async Task<ActionResult<IEnumerable<Entities.Notification>>> GetNotifications()
+        {
+            var notifications = await _repository.GetNotifications();
+            return Ok(notifications);
+        }
+    }
+}
