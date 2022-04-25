b9148ca78d1d21651121aafdae1a3836f8368532
marijal74 <lakic.marija98@gmail.com>
2022-04-25 10:44:53 +0200

    [file/feat] Test video file extension check

--- server/MovieVerseServer/Services/FileService/File.Tests/CheckFileFormat.cs
+++ server/MovieVerseServer/Services/FileService/File.Tests/CheckFileFormat.cs
@@ -56,6 +56,30 @@ namespace File.Tests
             Assert.NotNull(res);
             Assert.Equal(statusCodeResult.StatusCode, StatusCodes.Status400BadRequest);
         }
+        [Theory]
+        [InlineData(0)]
+        [InlineData(30000001)]
+        public async void InvalidFileSize_IVideo(long fSize)
+        {
+            // given DI arguments for construction of FileController and file name
+            var config = new ConfigurationBuilder()
+                                    .AddInMemoryCollection(_confOptions)
+                                    .Build();
+            var env = new Mock<IHostEnvironment>();
+            var logger = new Mock<ILogger<FileController>>();
+            var repo = new Mock<IFileRepository>();
+            env.Setup(e => e.ContentRootPath).Returns(_ContentRootPath);
+            var originalFileName = "foo.mp4";
+        
+            // when posting image of invalid size
+            var controller = new FileController(env.Object, logger.Object, config, repo.Object);
+            var res =  await controller.PostVideo("alice");
+            var statusCodeResult = (IStatusCodeActionResult)res;
+            
+            // then request fails
+            Assert.NotNull(res);
+            Assert.Equal(statusCodeResult.StatusCode, StatusCodes.Status400BadRequest);
+        }
 
 
         [Theory]
@@ -64,12 +88,32 @@ namespace File.Tests
         [InlineData(null)]
         [InlineData(".exe")]
         [InlineData(".doc")]
+        [InlineData(".svg")]
         public async void NotSupportedImageExtension(string ext)
         {
             // given extension of uploaded file
         
             // when checking not supported extension
             var extValidation = FileFormatValidator.ValidFileExt(ext);
+            
+            // then extension check returns false
+            Assert.False(extValidation);
+        }
+        [Theory]
+        [InlineData(".pdf")]
+        [InlineData("")]
+        [InlineData(null)]
+        [InlineData(".exe")]
+        [InlineData(".doc")]
+        [InlineData(".asf")]
+        [InlineData(".flv")]
+        
+        public async void NotSupportedVideoExtension(string ext)
+        {
+            // given extension of uploaded file
+        
+            // when checking not supported extension
+            var extValidation = FileFormatValidator.ValidFileExt(ext, true);
             
             // then extension check returns false
             Assert.False(extValidation);
