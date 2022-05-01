10c43e23f2135007e3ac04be086469a2937cc962
Nikola Stanojević <stanojevic.business@gmail.com>
2022-05-01 19:52:26 +0200

    Added controller and route for getting all notifications

---
+++ server/MovieVerseServer/Services/Notification/Notification.API/Controllers/NotificationController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Notification.Application.Contracts.Persistence;
+using Notification.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Notification.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotificationController : ControllerBase
+    {
+        private readonly INotificationRepository _repository;
+
+        public NotificationController(INotificationRepository repository)
+        {
+            _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<UserNotification>), StatusCodes.Status200OK)]
+        public async Task<ActionResult<IEnumerable<UserNotification>>> GetNotifications()
+        {
+            var notifications = await _repository.GetAll();
+            return Ok(notifications);
+        }
+    }
+}
