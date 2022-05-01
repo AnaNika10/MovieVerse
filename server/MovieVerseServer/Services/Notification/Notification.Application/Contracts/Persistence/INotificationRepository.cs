f3a6d4dc05012f7a799334dcdbd4d14659f5185b
Nikola Stanojević <stanojevic.business@gmail.com>
2022-05-01 19:49:17 +0200

    Added Application project for Notification service

---
+++ server/MovieVerseServer/Services/Notification/Notification.Application/Contracts/Persistence/INotificationRepository.cs
@@ -0,0 +1,15 @@
+﻿using Notification.Domain.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Notification.Application.Contracts.Persistence
+{
+    public interface INotificationRepository
+    {
+        Task<UserNotification> Get(int id);
+        Task<IEnumerable<UserNotification>> GetAll();
+        Task Add(UserNotification notification);
+        Task Delete(int id);
+        Task Update(UserNotification notification);
+    }
+}
