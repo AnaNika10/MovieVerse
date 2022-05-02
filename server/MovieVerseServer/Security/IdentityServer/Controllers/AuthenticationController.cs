bf9ed086f2f76412d67abba8eed7a9e7b6519768
Milica Radojicic <radojicicmilica98@gmail.com>
2022-05-02 20:42:52 +0200

    #22 Add user login

--- server/MovieVerseServer/Security/IdentityServer/Controllers/AuthenticationController.cs
+++ server/MovieVerseServer/Security/IdentityServer/Controllers/AuthenticationController.cs
@@ -2,10 +2,12 @@
 using IdentityServer.Controllers.Base;
 using IdentityServer.DTOs;
 using IdentityServer.Entities;
+using IdentityServer.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using System;
 using System.Threading.Tasks;
 
 namespace IdentityServer.Controllers
@@ -14,9 +16,12 @@ namespace IdentityServer.Controllers
     [ApiController]
     public class AuthenticationController : RegistrationControllerBase
     {
-        public AuthenticationController(ILogger<AuthenticationController> logger, IMapper mapper, UserManager<User> userManager, RoleManager<IdentityRole> roleManager)
+        private readonly IAuthenticationService _authService;
+
+        public AuthenticationController(ILogger<AuthenticationController> logger, IMapper mapper, UserManager<User> userManager, RoleManager<IdentityRole> roleManager, IAuthenticationService authService)
             : base(logger, mapper, userManager, roleManager)
         {
+            _authService = authService ?? throw new ArgumentNullException(nameof(authService));
         }
 
         [HttpPost("[action]")]
@@ -41,6 +46,20 @@ namespace IdentityServer.Controllers
         public async Task<IActionResult> RegisterAdministrator([FromBody] NewUserDto newUser)
         {
             return await RegisterNewUserWithRoles(newUser, new string[] { "Administrator" });
+        }
+
+        [HttpPost("[action]")]
+        [ProducesResponseType(typeof(AuthenticationModel), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        public async Task<IActionResult> Login([FromBody] UserCredentialsDto userCredentials)
+        {
+            var user = await _authService.ValidateUser(userCredentials);
+            if (user == null)
+            {
+                return Unauthorized();
+            }
+
+            return Ok(await _authService.CreateAuthenticationModel(user));
         }
     }
 }
