9695f0fc0144b1ae968d21562c57d7a73991ef96
popovic-olivera <o.popovic@outlook.com>
2022-05-13 22:20:37 +0200

    Initialized Feed tests

---
+++ server/MovieVerseServer/Services/FeedService/Feed.Tests/CommentControllerTests.cs
@@ -0,0 +1,33 @@
+﻿using Feed.Controllers;
+using Feed.DTOs.Comment;
+using Feed.Repository.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Feed.Tests
+{
+    public class CommentControllerTests
+    {
+        [Fact]
+        public async Task CreateComment_ReturnsComment_WithData()
+        {
+            // Arrange
+            CreateCommentDTO commentDTO = new()
+            {
+                UserId = "12345",
+                PostId = 4,
+                Text = "Creating comment test text",
+            };
+            var mockRepo = new Mock<ICommentRepository>();
+            var controller = new CommentController(mockRepo.Object);
+
+            // Act
+            var result = await controller.CreateComment(commentDTO);
+
+            // Assert
+            Assert.IsType<ActionResult<CommentDTO>>(result);
+        }
+    }
+}
